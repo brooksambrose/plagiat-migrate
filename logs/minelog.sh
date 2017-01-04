6c9514f764fa29f5cf15f92a8b220e6802d73cf5
Brooks Ambrose <brooksambrose@gmail.com>
2017-01-04 12:15:43 -0800

    docker build update

--- logs/minelog.sh
+++ logs/minelog.sh
@@ -4,7 +4,7 @@
 echo '
 Dependencies
 '
-cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep 'also installing the dependencies'
+cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep 'also installing the dependenc'
 
 # Installed packages
 echo '
@@ -23,3 +23,24 @@ echo '
 Non-zero exist status
 '
 cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep 'non-zero exit status'
+
+# Fail
+echo '
+Fail
+'
+cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep '[Ff][Aa][Ii][Ll]'
+
+# Error
+echo '
+Error
+'
+cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep '[Ee][Rr][Rr][Oo][Rr][^=.]'
+
+# Cannot find
+echo '
+Cannot find
+'
+cat '/Users/bambrose/Dropbox/GitHub/hos/plagiat/logs/bench.log' | grep 'cannot find'
+
+apt-get install -y apt-file && apt-file update
+apt-file search
