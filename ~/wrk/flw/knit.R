01af2d82df5c79e14d7a530e5fbf95265902e782
Brooks Ambrose <brooksambrose@gmail.com>
2017-01-11 15:02:20 -0800

    bibvex

--- ~/wrk/flw/knit.R
+++ ~/wrk/flw/knit.R
@@ -2,18 +2,19 @@
 rm(list=ls())
 cat('\014')
 require(knitr)
-for(ki in 1:9) {
+for(ki in 4) {
   ki<-sprintf('%02d',ki)
   knit(
     input=paste0('~/wrk/Rmd/',ki,'.Rmd')
     ,output=paste0('~/wrk/md/',ki,'.md')
   )
+
   call<-paste0(
     '/usr/lib/rstudio-server/bin/pandoc/pandoc +RTS -K512m -RTS ~/wrk/md/'
     ,ki
     ,'.md --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash --output ~/prd/tex/'
     ,ki
-    ,'.tex --latex-engine pdflatex --bibliography ~/prd/tex/00.bib --filter /usr/lib/rstudio-server/bin/pandoc/pandoc-citeproc'
+    ,'.tex --filter /usr/lib/rstudio-server/bin/pandoc/pandoc-citeproc'
     ,' && '
     ,'/usr/lib/rstudio-server/bin/pandoc/pandoc +RTS -K512m -RTS ~/prd/tex/'
     ,ki
@@ -24,4 +25,8 @@ for(ki in 1:9) {
   cat(call,'\n',sep='')
   system(call)
 }
-#system('cd ~/prd/tex && pdflatex ~/prd/tex/00.tex')
+if(F){
+system('cd ~/prd/tex && latexpand 00.tex > ~/prd/tex/0.tex')
+#/usr/lib/rstudio-server/bin/pandoc/pandoc +RTS -K512m -RTS ~/prd/tex/0.tex --to latex --from markdown+autolink_bare_uris+ascii_identifiers+tex_math_single_backslash --output ~/prd/+Ambrose_dissertation-draft_do-not-circulate.pdf --latex-engine pdflatex --bibliography ~/prd/tex/00.bib --filter /usr/lib/rstudio-server/bin/pandoc/pandoc-citeproc
+system('cd ~/prd/tex && pdflatex ~/prd/tex/00.tex')
+}
